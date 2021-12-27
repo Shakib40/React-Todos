8e8df53b5f6e03e804e2e2482cdef3a6cbd7b163
Shakib40 <shakibjilani@gmail.com>
2021-12-27 23:04:45 +0530

    Todos

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Grocery from './Grocery/Grocery';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Grocery />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
